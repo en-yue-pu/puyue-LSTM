be970f0c93e953e9c755586cb118f69cdc3e8e21
yue_pu <yue_pu@en-japan.com>
2023-07-10 19:51:09 +0900

    我只对预测未来感兴趣 修改代码

--- btc.py
+++ btc.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+from datetime import timedelta
 
 # Load the dataset
 data = pd.read_csv('BTC.csv')
@@ -24,25 +25,19 @@ data['Value'] = scaler.fit_transform(data['Value'].values.reshape(-1, 1))
 # Define a function to create a dataset for time series
 def create_dataset(dataset, look_back=1):
     dataX, dataY = [], []
-    for i in range(len(dataset) - look_back - 1):
+    for i in range(len(dataset) - look_back):
         a = dataset[i:(i + look_back), 0]
         dataX.append(a)
         dataY.append(dataset[i + look_back, 0])
     return np.array(dataX), np.array(dataY)
 
-# Split the data into training set and test set
-training_size = int(len(data) * 0.8)
-test_size = len(data) - training_size
-train_data, test_data = data.values[0:training_size, :], data.values[training_size:len(data), :]
-
-# Reshape the data and create time steps
+# Create training set
 look_back = 60  # we will use 60 days of data to predict the next day
+train_data = data.values
 X_train, y_train = create_dataset(train_data, look_back)
-X_test, y_test = create_dataset(test_data, look_back)
 
 # Reshape the input to be [samples, time steps, features]
 X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
-X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
 # Build the LSTM model
 model = Sequential()
@@ -55,24 +50,31 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 # Train the model
 model.fit(X_train, y_train, epochs=1, batch_size=1, verbose=2)
 
-# Use the model to make predictions
-train_predict = model.predict(X_train)
-test_predict = model.predict(X_test)
+# Use the model to predict the next 30 days
+future_days = 30
+predictions = []
+current_batch = train_data[-look_back:]
+current_batch = current_batch.reshape((1, 1, look_back))
+
+# Get the last valid date from the dataset
+last_date = data.dropna().index[-1]
+future_dates = [last_date]  # Start a new list for the future dates
+
+for i in range(future_days):
+    current_pred = model.predict(current_batch)[0]
+    predictions.append(current_pred)
+    current_batch = np.append(current_batch[:, :, 1:], [[current_pred]], axis=2)
+    last_date += timedelta(days=1)  # Update the last date
+    future_dates.append(last_date)  # Append the new date to the list
 
 # Invert the predictions to original scale
-train_predict = scaler.inverse_transform(train_predict)
-y_train = scaler.inverse_transform([y_train])
-test_predict = scaler.inverse_transform(test_predict)
-y_test = scaler.inverse_transform([y_test])
+predictions = scaler.inverse_transform(predictions)
 
-# Get the dates from the index
-train_dates = data.index[look_back:len(train_predict)+look_back]
-test_dates = data.index[len(train_predict)+(look_back*2)+1:len(data)-1]
+# Remove the first date from the future_dates list (it's the last date in the original data)
+future_dates = future_dates[1:]
 
 # Convert predictions to dataframes for easier handling
-train_df = pd.DataFrame(data=train_predict, index=train_dates, columns=['Value'])
-test_df = pd.DataFrame(data=test_predict, index=test_dates, columns=['Value'])
+predictions_df = pd.DataFrame(data=predictions, index=future_dates, columns=['Value'])
 
-# Save the predictions to CSV files
-train_df.to_csv('train_predictions.csv')
-test_df.to_csv('test_predictions.csv')
+# Save the predictions to a CSV file
+predictions_df.to_csv('future_predictions.csv')
